408f2bb3caf0a9dc27ccd2ba2d555e315153b16c
SantiCalabro <santi.calabro93@gmail.com>
2023-07-26 23:11:14 -0300

    adding images

--- camet/src/pages/productos/index.jsx
+++ camet/src/pages/productos/index.jsx
@@ -1,10 +1,45 @@
 import React from "react";
 import Layout from "../../layouts/index.jsx";
-export default function Productos() {
+import { graphql, Link } from "gatsby";
+import * as P from "../../styles/Products.module.css";
+import Img from "gatsby-image";
+export default function Productos({ data }) {
   return (
     <Layout>
-      {" "}
-      <div></div>
+      <div className={P.desktopHeader}>
+        <Img
+          fluid={data.desktopBanner.childImageSharp.fluid}
+          alt="Fotografía de galletitas con salamín"
+        />
+      </div>
+      <div className={P.responsiveHeader}>
+        <Img
+          fluid={data.mobileBanner.childImageSharp.fluid}
+          alt="Fotografía de galletitas con salamín"
+        />
+      </div>
+      <div className={P.titleContainer}>
+        <h3>• Nuestras variedades •</h3>
+      </div>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query {
+    desktopBanner: file(relativePath: { eq: "productos.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 1440, quality: 100) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    mobileBanner: file(relativePath: { eq: "productos_mobile.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 1440, quality: 100) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`;
