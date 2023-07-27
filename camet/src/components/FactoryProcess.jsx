7aafc4a08db6439f0cd45e54b625004f6d02f2d0
SantiCalabro <santi.calabro93@gmail.com>
2023-07-26 21:11:54 -0300

    homepage WIP

--- camet/src/components/FactoryProcess.jsx
+++ camet/src/components/FactoryProcess.jsx
@@ -31,10 +31,17 @@ export default function FactoryProcess() {
       </div>
       <div className={F.itemContainer}>
         {items.map((el, i) => (
-          <div className={F.item} key={i}>
+          <div
+            className={
+              el.title === "El proceso de elaboración" ? F.itemReverse : F.item
+            }
+            key={i}
+          >
             <img src={el.img} alt="" />
-            <span>{el.title}</span>
-            <p>{el.paragraph}</p>
+            <div className={F.text}>
+              <span>{el.title}</span>
+              <p>{el.paragraph}</p>
+            </div>
           </div>
         ))}
       </div>
--- camet/src/pages/index.jsx
+++ camet/src/pages/index.jsx
@@ -5,11 +5,12 @@ import "../styles/global.css";
 import { graphql, Link } from "gatsby";
 import Img from "gatsby-image";
 import FactoryProcess from "../components/FactoryProcess";
+import * as I from "../styles/index.module.css";
 import ProductsPreview from "../components/ProductsPreview";
 export default function Home({ data }) {
   return (
     <Layout>
-      <div>
+      <div className={I.desktopCarousel}>
         <Carousel>
           <Carousel.Item>
             <Link to="/productos/sinsal">
@@ -22,6 +23,24 @@ export default function Home({ data }) {
           <Carousel.Item>
             <Img
               fluid={data.slide01ql.childImageSharp.fluid}
+              alt="Tradición marplatense"
+            />
+          </Carousel.Item>
+        </Carousel>
+      </div>
+      <div className={I.responsiveCarousel}>
+        <Carousel>
+          <Carousel.Item>
+            <Link to="/productos/sinsal">
+              <Img
+                fluid={data.slide01ql_resp.childImageSharp.fluid}
+                alt="Línea sin sal"
+              />
+            </Link>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Img
+              fluid={data.slide02ql_resp.childImageSharp.fluid}
               alt="Tradición marplatense"
             />
           </Carousel.Item>
@@ -42,6 +61,20 @@ export const query = graphql`
       }
     }
     slide02ql: file(relativePath: { eq: "Slide02_desktop.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 1440, quality: 100) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    slide01ql_resp: file(relativePath: { eq: "sin_sal_mobile.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 1440, quality: 100) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    slide02ql_resp: file(relativePath: { eq: "lobo_mobile.jpg" }) {
       childImageSharp {
         fluid(maxWidth: 1440, quality: 100) {
           ...GatsbyImageSharpFluid
